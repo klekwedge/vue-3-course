dd78a4d997f54e515fe74d70b7c76e10ab6b4e48
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-26 14:50:22 +0300

    feat: add 3 lesson

---
+++ 3. Vue cli/03 Props/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <ul class="navbar-list">
+          <li class="navbar-item" v-for="link in links" :key="link.alias">
+            <a class="navbar-link" :href="link.url">{{ link.title }}</a>
+          </li>
+        </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComp',
+  props: {
+    links: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ 3. Vue cli/03 Props/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="navbar">
+    <div class="container">
+      <div class="navbar-content">
+        <a class="navbar-logo" href="/">{{title}}</a>
+        <ul class="navbar-list">
+          <li class="navbar-item" v-for="link in links" :key="link.alias">
+            <a class="navbar-link" :href="link.url">{{ link.title }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    links: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
