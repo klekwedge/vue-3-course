b34bed9cbcc7304edc873052fd7d08360b773ff9
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-30 16:11:13 +0300

    feat: add 4 lesson

---
+++ 3. Vue cli/04 Config/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="navbar">
+    <div class="container">
+      <div class="navbar-content">
+        <a class="navbar-logo" href="/">{{ title }}</a>
+        <ul class="navbar-list">
+          <li class="navbar-item" v-for="link in links" :key="link.alias">
+            <a class="navbar-link" :href="link.url">{{ link.title }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import { app, links } from '@/_config';
+export default {
+  name: 'HeaderComp',
+  data() {
+    return {
+      title: app.title,
+      links: links,
+    };
+  },
+};
+</script>
+
+<style></style>
