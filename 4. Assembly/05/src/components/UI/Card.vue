c694757d313f1a5c5af4681565470a44e98e4631
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-31 19:46:52 +0300

    feat: add 5 lesson

---
+++ 4. Assembly/05/src/components/UI/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <div class="card-img__wrapper" v-if="imgUrl">
+      <img class="card-img" :src="imgUrl" :alt="title" />
+    </div>
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }}</span>
+    <div class="card-body">
+      <slot></slot>
+
+      <router-link
+        v-if="link"
+        :to="link"
+        class="link"
+        style="display: block; margin-top: 16px;"
+        >See more</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    imgUrl: {
+      type: String
+    },
+    link: {
+      type: String
+    }
+  }
+}
+</script>
---
+++ 4. Assembly/05/src/pages/_itemAlias.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <div v-if="item">
+      <img :src="item.img" :alt="item.descr" />
+      <h1 class="title">{{ item.title }}</h1>
+      <p>{{ item.descr }}</p>
+      <div>
+        <router-link to="/" class="btn btnPrimary">Back to home</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import items from '@/seeders/items.js'
+export default {
+  data() {
+    return {
+      item: null
+    }
+  },
+  created() {
+    const alias = this.$route.params.itemAlias
+    const item = items.find(el => el.alias === alias)
+    this.item = item
+  }
+}
+</script>
