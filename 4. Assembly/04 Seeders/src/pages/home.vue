f87387498115b72e767af8917f8fb35aaa12de27
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-31 18:52:40 +0300

    feat: add 4 lesson

---
+++ 4. Assembly/04 Seeders/src/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="md-body">
+    <div class="card__wrapper" v-for="item in items" :key="item.id">
+      <Card :name="`${item.lvl} lvl`" :title="item.title" :imgUrl="item.img">
+        <!-- <img :src="item.img"> -->
+        {{ item.descr }}
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/UI/Card'
+import items from '@/seeders/items'
+
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      items: items
+    }
+  }
+}
+</script>
