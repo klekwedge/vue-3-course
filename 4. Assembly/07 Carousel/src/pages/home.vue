3e08d5810b01887ea5168531e459c97e6e4ba77d
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-01 15:18:14 +0300

    feat: add 7 lesson

---
+++ 4. Assembly/07 Carousel/src/pages/home.vue
@@ -0,0 +1,63 @@
+<template>
+    <carousel :settings="settings" :breakpoints="breakpoints">
+      <slide class="card__wrapper" v-for="item in items" :key="item.id">
+        <Card
+          :name="`${item.lvl} lvl`"
+          :title="item.title"
+          :imgUrl="item.img"
+          :link="`/${item.alias}`"
+        >
+          <template v-slot:body> {{ item.descr }}</template>
+          <template v-slot:footer>
+            <div class="card-stats">
+              <div
+                v-for="(stat, index) in item.info"
+                :key="index"
+                class="one-third"
+              >
+                <div class="stat-value">{{ stat.value }}</div>
+                <div class="stat">{{ stat.title }}</div>
+              </div>
+            </div>
+          </template>
+        </Card>
+      </slide>
+      <template #addons>
+        <navigation />
+      </template>
+    </carousel>
+</template>
+
+<script>
+import Card from '@/components/UI/Card'
+import items from '@/seeders/items'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
+export default {
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+    Card
+  },
+  data() {
+    return {
+      items: items,
+      settings: {
+        itemsToShow: 2,
+        wrapAround: true,
+        snapAlign: 'center'
+      },
+      breakpoints: {
+        300: {
+          itemsToShow: 1
+        },
+        700: {
+          itemsToShow: 2
+        }
+      }
+    }
+  }
+}
+</script>
