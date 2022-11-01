31befc7eb229a40c62dd607893dd421c88d9d43e
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-01 17:25:56 +0300

    feat: add 1 lesson

---
+++ 5. Notes Application 2/01 Form/src/pages/homePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="md-body">
+    <Form @onSubmit="handleSubmit" />
+  </div>
+</template>
+
+<script>
+import Form from '@/components/Notes/FormComp';
+
+export default {
+  components: {
+    Form,
+  },
+  methods: {
+    handleSubmit(note) {
+      console.log(note);
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
