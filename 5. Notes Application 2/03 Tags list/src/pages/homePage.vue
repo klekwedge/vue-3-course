9b681a2377202fccef0a84a003ce578fc68ff69d
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-01 19:40:54 +0300

    feat: add 3 lesson

---
+++ 5. Notes Application 2/03 Tags list/src/pages/homePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <Form @onSubmit="handleSubmit" />
+  <List @onRemove="handleRemove" :items="notes" />
+</template>
+
+<script>
+import Form from '@/components/Notes/FormComp';
+import List from '@/components/Notes/ListComp';
+
+export default {
+  components: {
+    Form,
+    List,
+  },
+  data() {
+    return { notes: ['task1', 'task2', 'task3']};
+  },
+  methods: {
+    handleSubmit(note) {
+      this.notes.push(note);
+    },
+    handleRemove(index) {
+      this.notes.splice(index, 1);
+    },
+  },
+};
+</script>
