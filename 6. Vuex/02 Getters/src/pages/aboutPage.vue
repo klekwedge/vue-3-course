9ac8cb5ec397eb44fd23a1a7e1f5421ee68141a0
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-02 12:04:38 +0300

    feat: add 2 lesson

---
+++ 6. Vuex/02 Getters/src/pages/aboutPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{ getUsersLength }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    getUsersLength() {
+      return this.$store.getters.getUsersLength;
+    },
+  },
+};
+</script>
---
+++ 6. Vuex/02 Getters/src/pages/homePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="user in getUsers" :key="user.id">
+      <p>{{ user.id }}</p>
+      <p>{{ user.name }}</p>
+      <p>{{ user.admin ? 'Это админ' : 'Обычный пользователь' }}</p>
+    </li>
+  </ul>
+  <p>{{ getUsersLength }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    getUsers() {
+      return this.$store.getters.getUsers;
+    },
+    getUsersLength() {
+      return this.$store.getters.getUsersLength;
+    },
+  },
+};
+</script>
