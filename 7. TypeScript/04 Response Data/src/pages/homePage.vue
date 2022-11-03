744345ec28f8bb6a858128a0ff15b33c2eaed4ac
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-03 14:01:54 +0300

    feat: add 4 lesson

---
+++ 7. TypeScript/04 Response Data/src/pages/homePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Home</p>
+</template>
+
+<script lang="ts">
+import { onMounted } from 'vue';
+import DataService from '@/services/DataService';
+import ResponseData from '@/types/ResponseData';
+// defineComponent,
+export default {
+  setup() {
+    onMounted(() => getPeople());
+
+    const getPeople = () => {
+      DataService.getAll()
+        .then((res: ResponseData) => console.log(res.data))
+        .catch((e: Error) => console.log(e));
+    };
+  },
+};
+</script>
