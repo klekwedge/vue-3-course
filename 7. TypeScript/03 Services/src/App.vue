0c86a8e3d16137b62ebe0a3ca142fad5ef03e8b8
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-03 13:47:58 +0300

    feat: add 3 lesson

---
+++ 7. TypeScript/03 Services/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wapper">
+    <Header />
+    <div class="wrapper-content">
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Header from '@/components/HeaderComp.vue';
+import Footer from '@/components/FooterComp.vue';
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
