f3fffc4dbfb949f2a3b9eb75b4c68f7d2cc18392
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-03 13:18:58 +0300

    feat: add 1 lesson

---
+++ 7. TypeScript/01 Interfaces/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wapper">
+    <Header />
+    <div class="wrapper-content">
+      <div class="container">
+        {{ getName }}
+        <router-view />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Header from '@/components/HeaderComp.vue';
+import Footer from '@/components/FooterComp.vue';
+
+interface IPerson {
+  id: number;
+  title: string;
+  male: boolean;
+}
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      wizzard: {
+        id: 1183,
+        title: 'Wizzard',
+        male: true,
+      } as IPerson,
+    };
+  },
+  computed: {
+    getName(): string {
+      return this.wizzard.title;
+    },
+  },
+  methods: {
+    setName(title: string) {
+      this.wizzard.title = title;
+    },
+  },
+};
+</script>
